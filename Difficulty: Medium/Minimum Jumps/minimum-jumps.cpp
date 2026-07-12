5e0e9efd4ed8dd4b9560915122cbfc0b398a04ff
Ashwani kumar mishra <ashwanimishra17617@gmail.com>
2026-07-12 21:00:37 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimum Jumps/minimum-jumps.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    int minJumps(vector<int>& arr) {
+        // code here
+        int n=arr.size();
+        int maxindex=arr[0];
+        int step=arr[0];
+        int jump=1;
+        if(n==1)return 0;
+        if(arr[0]==0)return -1;
+        for(int i=1;i<n;i++){
+            if(i==n-1) return jump;
+            maxindex=max(maxindex,i+arr[i]);
+            step--;
+            if(step==0){
+                jump++;
+                if(i==maxindex) return -1;
+                step=maxindex-i;
+            }
+        }
+        return -1;
+        
+    }
+};
