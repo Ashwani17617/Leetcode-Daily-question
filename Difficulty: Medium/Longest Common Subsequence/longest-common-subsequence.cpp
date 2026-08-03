1ffedcba7487fbf2083dfc1ca02a276b6940d726
Ashwani kumar mishra <ashwanimishra17617@gmail.com>
2026-08-03 17:42:34 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Common Subsequence/longest-common-subsequence.cpp
+++ Difficulty: Medium/Longest Common Subsequence/longest-common-subsequence.cpp
@@ -1,5 +1,32 @@
 class Solution {
   public:
+    int lcs(string &s1, string &s2) {
+        // code here
+        int n=s1.size();
+        int m=s2.size();
+        vector<int> prev(m+1,0), curr(m+1,0);
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s1[i-1]==s2[j-1]){
+                    curr[j]=1+prev[j-1];
+                }else{
+                    curr[j]=max(prev[j],curr[j-1]);
+                }
+                
+            }
+            prev=curr;
+            vector<int>curr(m+1,0);
+        }
+        return prev[m];
+    }
+};
+
+/*
+Recursion stack space overhead is the extra memory your computer uses to keep track of active function calls
+So,Yes, tabulation is an optimized approach because it completely removes the recursion stack space overhead.
+
+class Solution {
+  public:
     
     int sol(vector<vector<int>> &dp,int index1,int index2,string &s1, string &s2){
         
@@ -18,3 +45,5 @@ class Solution {
         return sol(dp,s1.size()-1,s2.size()-1,s1,s2);
     }
 };
+
+*/
